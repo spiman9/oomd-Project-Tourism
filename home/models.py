490c9a7ac26ebc50ea41caf1504c07aae090a26b
UtkarshTopinkatti <utkarshtopinkatti@gmail.com>
2023-01-28 19:12:45 +0530

    complete

--- home/models.py
+++ home/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class contactus(models.Model):
+    email=models.EmailField()
+    fname=models.TextField()
+    lname=models.TextField()
+    country=models.TextField()
+    description=models.TextField()
+    def __str__(self):
+        return self.email
--- home/views.py
+++ home/views.py
@@ -4,6 +4,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate
 from django.core.mail import send_mail
 from django.conf import settings
+from django.contrib.auth.models import User,auth
+from .models import contactus as CU
 # Create your views here.
 def home(request):
     return render(request ,"main.html")
@@ -22,8 +24,44 @@ def contactussub(request):
         htmlgen = '<p>Message from <strong>'+fname+' '+lname+' from '+country+ ' </strong>'+msg+'</p>'
         
         send_mail('Feedback ,',msg,email,[settings.EMAIL_HOST_USER],fail_silently=False,html_message=htmlgen)
+        b=CU(email=email,fname=fname,lname=lname,country=country,description=sub)
+        b.save()
         messages.success(request,"Successfull Messag send")
     else:
         print("else")
     return render(request,"contactus.html")
 
+def register(request):
+    return render(request,"register.html")
+def login(request):
+    return render(request,"login.html")
+
+def filllogin(request):
+    if request.method=="POST":
+        user=auth.authenticate(username=request.POST['uname'],password=request.POST["psw"])
+        if user is not None:
+            auth.login(request,user)
+            return redirect("/")
+        else:
+            print("User not present")
+            return render(request,"login.html")
+    else:
+        return render(request,"login.html")
+
+def fillregister(request):
+    if request.method=="POST":
+        if User.objects.filter(username=request.POST["uname"]).exists():
+            messages.error(request,"Username already exist")
+            return render(request,"register.html")
+        if User.objects.filter(email=request.POST["email"]).exists():
+            messages.error(request,"Email already exist")
+            return render(request,"register.html")
+        if(request.POST["psw1"]!=request.POST["psw2"]):
+            messages.error(request,"Pls enter the correct password")
+            return render(request,"register.html")
+        
+        x=User.objects.create_user(username=request.POST["uname"],email=request.POST["email"],first_name=request.POST["fname"],last_name=request.POST["lname"],password=request.POST["psw1"])
+        return render(request,"main.html")
+def logout(request):
+    auth.logout(request)
+    return redirect("/")
