6adf46b5984abb9b1047319202e280b0c2cf71c2
UtkarshTopinkatti <utkarshtopinkatti@gmail.com>
2023-02-05 11:50:34 +0530

    finalcom

--- home/admin.py
+++ home/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import contactus
-from .models import India , Dubai1 , Singapore
+from .models import India , Dubai1 , Singapore,specialoffer
 
 # Register your models here.
 admin.site.register(India)
 admin.site.register(Dubai1)
 admin.site.register(Singapore)
 admin.site.register(contactus)
+admin.site.register(specialoffer)
